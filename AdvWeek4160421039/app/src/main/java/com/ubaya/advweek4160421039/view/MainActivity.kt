320e3b5d87fff92d6537989f1e1fa7efda3f490a
Jennie03 <s160421039@student.ubaya.ac.id>
2024-03-20 21:29:03 +0700

    slide 41

--- AdvWeek4160421039/app/src/main/java/com/ubaya/advweek4160421039/view/MainActivity.kt
+++ AdvWeek4160421039/app/src/main/java/com/ubaya/advweek4160421039/view/MainActivity.kt
@@ -1,12 +1,13 @@
-package com.ubaya.advweek4160421039
+package com.ubaya.advweek4160421039.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ubaya.advweek4160421039.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //test
+
     }
 }
